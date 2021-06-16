1f8fdcd76b4d21599299e7a3e8ce06e2365df577
Mikhael <mikhaelapg00@gmail.com>
2021-06-17 00:20:35 +0700

    routes and controller

---
+++ app/Controllers/Coba.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controllers;
+
+class Coba extends BaseController
+{
+	public function index()
+	{
+		echo 'Ini controller coba method index';
+	}
+
+	public function about($nama = '') 
+	{
+		echo "Halo, nama saya $nama.";
+	}
+}
