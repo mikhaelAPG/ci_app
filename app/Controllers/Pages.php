c276f1fb67e4899b2d7d479dc37bc71c7d32562a
Mikhael <mikhaelapg00@gmail.com>
2021-06-17 00:47:22 +0700

    views

---
+++ app/Controllers/Pages.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controllers;
+
+class Pages extends BaseController
+{
+	public function index()
+	{   
+        $data = [
+            'title' => 'Home | CI4App'
+        ];
+        echo view('layout/header', $data);
+		echo view('pages/home');
+        echo view('layout/footer');
+	}
+
+    public function about()
+    {
+        $data = [
+            'title' => 'About | CI4App'
+        ];
+        echo view('layout/header', $data);
+        echo view('pages/about');
+        echo view('layout/footer');
+    }
+}
---
+++ app/Views/pages/about.php
@@ -0,0 +1,8 @@
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>About Me</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi voluptates itaque debitis? Soluta, ex? Totam veritatis praesentium nobis illo impedit recusandae, maiores, temporibus mollitia incidunt optio, sint autem quae architecto!</p>
+        </div>
+    </div>
+</div>
