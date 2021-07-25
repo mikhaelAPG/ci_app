d34a7305e6e391afb3f9c1e07aa83b907b16c848
Mikhael <mikhaelapg00@gmail.com>
2021-07-25 11:49:46 +0700

    view layouts fix

---
+++ app/Views/pages/about.php
@@ -0,0 +1,12 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>About Me</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi voluptates itaque debitis? Soluta, ex? Totam veritatis praesentium nobis illo impedit recusandae, maiores, temporibus mollitia incidunt optio, sint autem quae architecto!</p>
+        </div>
+    </div>
+</div>
+<?= $this->endSection(); ?> 
